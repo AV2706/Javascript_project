028c823dc9b18c469d439f11f48140ff86fcd4b1
bala-benny <balabenny2020@gmail.com>
2025-11-08 23:52:51 +0530

    cursor image,movement ,score etc

---
+++ script.js
@@ -0,0 +1,55 @@
+const ball = document.getElementById("ball");         
+const scoreDisplay = document.getElementById("score"); 
+const catchSound = document.getElementById("catchSound"); 
+
+const catCursor = document.createElement("img");       
+catCursor.src = "cat.jpeg";   
+catCursor.id = "cat-cursor";                        
+document.body.appendChild(catCursor);                
+
+let score = 0;
+document.addEventListener("mousemove", (e) => {
+  catCursor.style.left = e.pageX + "px"; 
+  catCursor.style.top = e.pageY + "px";  
+});
+
+let ballX = 100, ballY = 100; 
+let speedX = 4, speedY = 3;  
+
+setInterval(() => {  
+  ballX += speedX;  
+  ballY += speedY; 
+   
+  if (ballX <= 0 || ballX + 60 >= window.innerWidth) speedX = -speedX;
+  if (ballY <= 0 || ballY + 60 >= window.innerHeight) speedY = -speedY;
+
+ 
+
+  ball.style.left = `${ballX}px`;
+  ball.style.top = `${ballY}px`;
+
+
+const catRect = catCursor.getBoundingClientRect();
+const ballRect = ball.getBoundingClientRect();
+const overlap =
+catRect.left < ballRect.right &&
+catRect.right > ballRect.left &&
+catRect.top < ballRect.bottom &&
+catRect.bottom > ballRect.top;
+
+if (overlap) {
+score++;
+scoreDisplay.textContent = score;
+catchSound.play();
+speedX *= 1.05;
+speedY *= 1.05;
+}
+}, 20);
+
+
+
+
+
+  
+ 
+
